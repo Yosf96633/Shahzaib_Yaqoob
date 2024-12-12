4d5755ca9036ae908ff0c461653529c89c3f859d
Yosf96633 <yosf96633@gmail.com>
2024-12-12 23:08:14 +0500

    :)

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import champ from "../assets/champ.jpg";
-import fakeCV from "../assets/fakeCV.pdf"
+import champ from "../assets/champ.jpeg";
+import fakeCV from "../assets/fakeCV.pdf";
 import { TypeAnimation } from "react-type-animation";
 import ShinyEffect from "./ShinyEffect";
 import { AiOutlineGithub, AiOutlineLinkedin } from "react-icons/ai";
-import { SiLeetcode , SiKaggle , SiX , SiThreads  } from "react-icons/si";
+import { SiLeetcode, SiKaggle, SiX, SiThreads } from "react-icons/si";
 import { motion } from "framer-motion";
 
 const Hero = () => {
@@ -18,12 +18,7 @@ const Hero = () => {
           transition={{ duration: 1 }}
         >
           <TypeAnimation
-            sequence={[
-              "AI/ML Engeenier",
-              3000,
-              "Freelancer",
-              3000,
-            ]}
+            sequence={["AI/ML Engeenier", 3000, "Freelancer", 3000]}
             speed={50}
             repeat={Infinity}
             className="font-bold text-gray-400 text-xl md:text-5xl italic- mb-4"
@@ -47,11 +42,11 @@ const Hero = () => {
             transition={{ duration: 1, delay: 1 }}
             className="text-gray-300 max-w-[300px] md:max-w-[500px] md:text-2xl text-lg mb-6"
           >
-            A recent graduate in Computer
-            Science with a passion for Artificial Intelligence (AI) and Data
-            Science. Throughout my academic journey, I’ve honed my skills in
-            programming, machine learning, and data analysis, combining
-            theoretical knowledge with practical project experience.{" "}
+            A recent graduate in Computer Science with a passion for Artificial
+            Intelligence (AI) and Data Science. Throughout my academic journey,
+            I’ve honed my skills in programming, machine learning, and data
+            analysis, combining theoretical knowledge with practical project
+            experience.{" "}
           </motion.p>
 
           <motion.div
@@ -62,28 +57,34 @@ const Hero = () => {
             className="flex flex-row items-center gap-6 my-4 md:mb-0"
           >
             <motion.a
-  whileHover={{
-    scale: 1.05,
-    boxShadow: "0px 0px 8px rgba(0, 0, 0, 0.3)",
-  }}
-  href={fakeCV} // Path to your PDF
-  download="MyCV.pdf" // Optional: Specify the download file name
-  className="z-10 cursor-pointer font-bold text-gray-200 md:w-auto p-4 border
+              whileHover={{
+                scale: 1.05,
+                boxShadow: "0px 0px 8px rgba(0, 0, 0, 0.3)",
+              }}
+              href={fakeCV} // Path to your PDF
+              download="MyCV.pdf" // Optional: Specify the download file name
+              className="z-10 cursor-pointer font-bold text-gray-200 md:w-auto p-4 border
                                    border-purple-400 rounded-xl"
->
-  Download CV
-</motion.a>
-
+            >
+              Download CV
+            </motion.a>
           </motion.div>
         </motion.div>
 
         <motion.img
           src={champ}
           className="w-[300px] md:w-[450px] rounded-full"
-          initial={{ opacity: 0, scale: 0.8 }}
-          whileInView={{ opacity: 1, scale: 1 }}
-          viewport={{ once: true }}
-          transition={{ duration: 1 }}
+          initial={{ opacity: 0, scale: 0.8, rotateY: 0 }} // Initial state: invisible, scaled down, and no rotation
+          whileInView={{
+            opacity: 1,
+            scale: 0.85, // Slightly smaller size during animation
+            rotateY: 360 * 2, // Rotate the image 360 degrees in 3D space
+          }}
+          viewport={{ once: true }} // Animate once when the image enters the viewport
+          transition={{
+            duration: 2.2, // Duration of the animation (longer time for slower animation)
+            ease: "easeInOut", // Smooth easing for a gradual start and end
+          }}
         />
       </div>
 
@@ -126,13 +127,13 @@ const Hero = () => {
             whileHover={{ scale: 1.2 }}
             href="https://x.com/shahzaib_mal_ik?s=08"
           >
-            <SiX className=""/>
+            <SiX className="" />
           </motion.a>
           <motion.a
             whileHover={{ scale: 1.2 }}
             href="https://www.threads.net/@shahzaib_mal_ik"
           >
-            <SiThreads className=""/>
+            <SiThreads className="" />
           </motion.a>
         </div>
       </motion.div>
--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -1,6 +1,15 @@
 import React, { useState } from "react";
 import { motion, useMotionValue, useTransform } from "framer-motion";
+import cr1 from "../assets/cr1.png";
+import cr2 from "../assets/cr2.png";
 
+import cr3 from "../assets/cr3.png";
+
+import cr4 from "../assets/cr4.png";
+
+import cr5 from "../assets/cr5.png";
+
+import cr6 from "../assets/cr6.png";
 const Slider = () => {
   const [cards, setCards] = useState(cardData);
 
@@ -40,7 +49,7 @@ const Card = ({ id, url, setCards, cards }) => {
     <motion.img
       src={url}
       alt="Placeholder alt"
-      className="h-96 w-72 origin-bottom rounded-lg bg-white object-cover hover:cursor-grab active:cursor-grabbing"
+      className="h-[28rem] w-[36rem] max-sm:h-80 max-sm:w-[24rem] origin-bottom rounded-lg bg-white object-cover hover:cursor-grab active:cursor-grabbing"
       style={{
         gridRow: 1,
         gridColumn: 1,
@@ -67,34 +76,26 @@ export default Slider;
 const cardData = [
   {
     id: 1,
-    url: "https://images.unsplash.com/photo-1542291026-7eec264c27ff?q=80&w=2370&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    url: cr1,
   },
   {
     id: 2,
-    url: "https://images.unsplash.com/photo-1512374382149-233c42b6a83b?q=80&w=2235&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    url: cr2,
   },
   {
     id: 3,
-    url: "https://images.unsplash.com/photo-1539185441755-769473a23570?q=80&w=2342&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    url: cr3,
   },
   {
     id: 4,
-    url: "https://images.unsplash.com/photo-1549298916-b41d501d3772?q=80&w=2224&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    url: cr4,
   },
   {
     id: 5,
-    url: "https://images.unsplash.com/photo-1516478177764-9fe5bd7e9717?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    url: cr5,
   },
   {
     id: 6,
-    url: "https://images.unsplash.com/photo-1570464197285-9949814674a7?q=80&w=2273&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    id: 7,
-    url: "https://images.unsplash.com/photo-1578608712688-36b5be8823dc?q=80&w=2187&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    id: 8,
-    url: "https://images.unsplash.com/photo-1505784045224-1247b2b29cf3?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    url: cr6,
   },
 ];
