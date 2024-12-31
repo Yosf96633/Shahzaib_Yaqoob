cc7f6f84935c098735594ad54849aa6d444aa775
Yosf96633 <yosf96633@gmail.com>
2024-12-31 15:04:19 +0500

    Changes

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -85,7 +85,7 @@ const Contact = () => {
 
               <div className="bg-gray-800/40 p-5 rounded-lg">
                 <h3 className="md:text-4xl text-2xl font-semibold text-white">
-                  300
+                  500
                   <span>+</span>
                 </h3>
                 <p className="text-xs md:text-base">
